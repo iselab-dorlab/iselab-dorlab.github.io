ce470a3d1c0b8cafe8694c41b783415ce751b21d
Thien Nguyen Duc <ndthien98@gmail.com>
2024-08-07 01:33:16 +0700

    feat: change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import ScrollTop from './components/ScrollTop';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollTop from "./components/ScrollTop";
 
-import MarkdownPage from './pages/MarkdownPage';
-import Home from './pages/Home';
-import Members from './pages/Members';
-import ResearchTopic from './pages/ResearchTopic';
-import Publications from './pages/Publications';
-import Contact from './pages/Contact';
-import AboutUs from './pages/AboutUs';
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import MarkdownPage from "./pages/MarkdownPage";
+import Members from "./pages/Members";
+import Publications from "./pages/Publications";
+import ResearchTopic from "./pages/ResearchTopic";
 
 const App = () => {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <ScrollToTop />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/members" element={<Members />} />
-                    <Route path="/researchtopic" element={<ResearchTopic />} />
-                    <Route path="/publications" element={<Publications />} />
-                    <Route path="/aboutus" element={<AboutUs />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/news/:filename" element={<MarkdownPage />} />
-                </Routes>
-                <Footer />
-                <ScrollTop />
-            </div>
-        </Router>
-    );
-}
+  return (
+    <div className="App">
+      <Header />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/research-topic" element={<ResearchTopic />} />
+        <Route path="/publication" element={<Publications />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/news/:filename" element={<MarkdownPage />} />
+      </Routes>
+      <Footer />
+      <ScrollTop />
+    </div>
+  );
+};
 
 export default App;
