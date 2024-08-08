dd1354f943426f71bebb20a75f3ca55ed518767c
ai4se-iselab <iselab.bkai@gmail.com>
2024-08-08 13:36:18 +0900

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Footer from "@/views/components/Footer";
 import Header from "@/views/components/Header";
+import ScrollTop from "@/views/components/ScrollTop";
+import ScrollToTop from "@/views/components/ScrollToTop";
 import "./global.css";
 import Head from 'next/head';
 
@@ -10,8 +12,10 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Laboratory of Intelligent Software Engineering and Data-driven Operation Research</title>
       </Head>
+      <ScrollToTop />
       <Header />
       <Component {...pageProps} />
+      <ScrollTop />
       <Footer />
     </>
   );
